a7b266898ea2662142a8d3cfc35390cba193a307
Casey <casey@simplethemes.com>
2012-04-01 18:16:47 -0500

    IE Bugfixes
    Hardened PIE.php path location for IE browsers and potential quirks
    mode fix.

--- skeleton/header.php
+++ skeleton/header.php
@@ -13,10 +13,13 @@
 <!--[if lt IE 7 ]><html class="ie ie6" <?php language_attributes();?>> <![endif]-->
 <!--[if IE 7 ]><html class="ie ie7" <?php language_attributes();?>> <![endif]-->
 <!--[if IE 8 ]><html class="ie ie8" <?php language_attributes();?>> <![endif]-->
-<!--[if (gte IE 9)|!(IE)]><!--><html <?php language_attributes();?>> <!--<![endif]-->
+<!--[if IE 9 ]><html class="ie ie9" <?php language_attributes();?>> <![endif]-->
+<!--[if (gte IE 10)|!(IE)]><!--><html <?php language_attributes();?>> <!--<![endif]-->
 
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+
 <title><?php
 	// Detect Yoast SEO Plugin
 	if (defined('WPSEO_VERSION')) {
@@ -49,6 +52,24 @@
 <!--[if lt IE 9]>
 	<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
 <![endif]-->
+
+<!-- Add CSS3 Rules here for IE 7-9
+================================================== -->
+
+<!--[if IE]>
+<style type="text/css">
+html.ie #navigation,
+html.ie a.button,
+html.ie .cta,
+html.ie .wp-caption,
+html.ie #breadcrumbs,
+html.ie a.more-link,
+html.ie .gallery .gallery-item img,
+html.ie .gallery .gallery-item img.thumbnail,
+html.ie .widget-container,
+html.ie #author-info {behavior: url("<?php echo get_template_directory_uri();?>/PIE.php");position: relative;}</style>
+<![endif]-->
+
 
 <!-- Mobile Specific Metas
 ================================================== -->

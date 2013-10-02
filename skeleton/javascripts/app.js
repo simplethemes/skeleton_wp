b8ccf8192b369183aca638ea1ef5c58e76f2fd0f
Casey <casey@simplethemes.com>
2013-10-01 23:11:29 -0500

    added support for multiple tabs

--- skeleton/javascripts/app.js
+++ skeleton/javascripts/app.js
@@ -1,15 +1,15 @@
-/* 
+/*
 * Skeleton V1.0.3
 * Copyright 2011, Dave Gamache
 * www.getskeleton.com
 * Free to use under the MIT license.
 * http://www.opensource.org/licenses/mit-license.php
 * 7/17/2011
-*/	
+*/
 
 jQuery(document).ready(function($) {
 
-	
+
 		/* Superfish
 	================================================== */
 	$(function(){ // run after page loads
@@ -19,35 +19,35 @@ jQuery(document).ready(function($) {
 			.end()
 			.superfish({autoArrows	: true});
 	});
-	
+
 	// Forum Login
-	
+
 	$(function(){ // run after page loads
-		$("#toggle").click(function() { 
+		$("#toggle").click(function() {
 	    // hides matched elements if shown, shows if hidden
 	    $("#login-form").slideToggle();
 	  });
 	});
 
 	// Style Tags
-	
+
 	$(function(){ // run after page loads
 		$('p.tags a')
 		.wrap('<span class="st_tag" />');
 	});
-	
+
 
 	// Toggle Slides
-	
+
 	$(function(){ // run after page loads
-			$(".toggle_container").hide(); 
+			$(".toggle_container").hide();
 			//Switch the "Open" and "Close" state per click then slide up/down (depending on open/close state)
 			$("p.trigger").click(function(){
 				$(this).toggleClass("active").next().slideToggle("normal");
 				return false; //Prevent the browser jump to the link anchor
 			});
 	});
-	
+
 	// valid XHTML method of target_blank
 	$(function(){ // run after page loads
 		$('a[rel*=external]').click( function() {
@@ -63,29 +63,30 @@ jQuery(document).ready(function($) {
 	tabs.each(function(i) {
 		//Get all tabs
 		var tab = $(this).find('> li > a');
-		$("ul.tabs li:first").addClass("active").fadeIn('fast'); //Activate first tab
-		$("ul.tabs li:first a").addClass("active").fadeIn('fast'); //Activate first tab
-		$("ul.tabs-content li:first").addClass("active").fadeIn('fast'); //Activate first tab
-		
+		$("ul.tabs li:first-child").addClass("active").fadeIn('fast'); //Activate first tab
+		$("ul.tabs li:first-child a").addClass("active").fadeIn('fast'); //Activate first tab
+		$("ul.tabs-content li:first-child").addClass("active").fadeIn('fast'); //Activate first tab
+
+
 		tab.click(function(e) {
-			
+
 			//Get Location of tab's content
 			var contentLocation = $(this).attr('href') + "Tab";
-			
+
 			//Let go if not a hashed one
 			if(contentLocation.charAt(0)=="#") {
-			
+
 				e.preventDefault();
-			
+
 				//Make Tab Active
 				tab.removeClass('active');
 				$(this).addClass('active');
-				
+
 				//Show Tab Content & add active class
 				$(contentLocation).show().addClass('active').siblings().hide().removeClass('active');
-				
-			} 
+
+			}
 		});
-	}); 
-	
+	});
+
 });

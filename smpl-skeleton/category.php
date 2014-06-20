439108d98c098ea13645e8f4fc1e9a566baa199a
Casey <casey@simplethemes.com>
2014-06-20 00:24:49 -0500

    Add category description selector

--- smpl-skeleton/category.php
+++ smpl-skeleton/category.php
@@ -11,18 +11,12 @@ get_header();
 do_action('skeleton_before_content');
 
 ?>
-
-<h1><?php
-		printf( __( 'Category Archives: %s', 'smpl' ), single_cat_title( '', false ) );
-	?></h1>
+	<h1><?php printf( __( 'Category Archives: %s', 'smpl' ), single_cat_title( '', false ) ); ?></h1>
 	<?php
-		$category_description = category_description();
-		if ( ! empty( $category_description ) )
-			echo '' . $category_description . '';
-	/* Run the loop for the category page to output the posts.
-	 * If you want to overload this in a child theme then include a file
-	 * called loop-category.php and that will be used instead.
-	 */
+	$category_description = category_description();
+	if ( ! empty( $category_description ) ) {
+		echo '<div class="category-description>' . $category_description . '</div>';
+	}
 	get_template_part( 'loop', 'category' );
 	do_action('skeleton_after_content');
 	get_sidebar();
